366bcbee06deefba0049ac4c3f95dd7541345b48
Stefan Ilie <ilie.stefan92@yahoo.com>
2015-10-10 17:12:43 +0300

    stuff

--- server/methods/events.js
+++ server/methods/events.js
@@ -1,15 +1,37 @@
 Meteor.methods({
-  'createEvent': function(usr_id, title, date, location, poster){
-    var eventId = Events.insert({
+  'createEvent': function(usr_id, title, date, location) {
+    var uniqueID = new Meteor.Collection.ObjectID();
+    Events.insert({
+      _id: uniqueID,
       usr_id: usr_id,
       title: title,
       date: date,
       location: location,
-      poster: poster
     })
-    return eventId;
+    return uniqueID;
   },
-  'editEvent': function(){
+  'editEvent': function() {
     console.log("vaporware");
+  },
+  'createTicket': function(eventId, spots, price, end_date, ticket) {
+    var object = Events.findOne({
+      _id: eventId
+    });
+    Events.update({
+      _id: eventId
+    }, {
+      usr_id: object['usr_id'],
+      title: object['title'],
+      date: object['date'],
+      location: object['location'],
+      ticket_type: {
+        spots: spots,
+        price: price,
+        end_date: end_date,
+        ticket: {
+          uid: new Meteor.Collection.ObjectID(),
+          sold: false
+      }
+    })
   }
 })
