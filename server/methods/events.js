eb5a1a998567b901e9182cf79857794444e6dd06
Stefan Ilie <ilie.stefan92@yahoo.com>
2015-10-10 15:47:29 +0300

    add events pub/sub, working on createtiCKets

--- server/methods/events.js
+++ server/methods/events.js
@@ -1,11 +1,18 @@
 Meteor.methods({
   'createEvent': function(usr_id, title, date, location, poster){
-    Events.insert({
+    var eventId = Events.insert({
       usr_id: usr_id,
       title: title,
       date: date,
       location: location,
       poster: poster
     })
+    return eventId;
   },
+  'editEvent': function(){
+    console.log("vaporware");
+  },
+  'createTicket': function(eventId, ){
+
+  }
 })
