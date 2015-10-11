1418a77a5099412cbb0d3a966f8607f45999dc05
Stefan Ilie <ilie.stefan92@yahoo.com>
2015-10-11 12:53:29 +0300

    add frontend stuff

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -3,9 +3,20 @@ Template.home.rendered = function() {
 };
 
 Template.home.helpers({
-	events: function() {
-		console.log("ma cac in ciorba2");
-		console.log(Events.find());
-		return Events.find();
-	}
+  events: function() {
+    console.log("ma cac in ciorba2");
+    console.log(Events.find());
+    return Events.find();
+  }
+});
+
+Template.home.events({
+  'click #buttonDetail': function(event) {
+		var text = "Location: "+this.location+"\nDate: "+this.date+"\nSpots left: "+this.ticket_type.spots+"\nSale end: "+this.ticket_type.end_date+"\nPrice: "+this.ticket_type.price;
+    swal({
+      title: this.title,
+      text: text,
+      html: true
+    });
+  }
 });
